76604e160d3b2a80ed71f2acb4447342ef14a070
Miguel Labrador <github4138@mlsdm.com>
2022-01-02 19:10:51 +0100

    Terminado ejercicio 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,32 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    let hey = '';
+    let error = "ERROR";
+
+    if (string == '') {
+
+        return hey;
+
+    } else if (num == 0) {
+
+        return hey;
+    }
+
+    else if (num < 0) {
+
+        return error;
+
+    } else {
+
+        for (let i = 0; i < num; i++) {
+
+            hey += string;
+
+        }
+
+        return hey;
+
+    }
 };
 
 // Do not edit below this line
