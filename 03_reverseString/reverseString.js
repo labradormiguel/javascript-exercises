467802e4833df43081426b0b82e26bf2b1bd0612
Miguel Labrador <github4138@mlsdm.com>
2022-01-02 20:35:04 +0100

    Terminado ejercicio 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
 const reverseString = function(str) {
+    let reves = [];
+    let resultado = '';
+
+    for (let i = str.length-1; i >= 0; i--) {
+        reves.push(str[i]);
+    }
+
+    for (let i = 0; i < reves.length; i++) {
+        resultado += reves[i];
+        
+    }
+
+    return resultado;
 
 };
 
