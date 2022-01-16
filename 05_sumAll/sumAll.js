7526d2a3af1ac3ac3d9d5e33fe4cdc46d7a15443
Miguel Labrador <github4138@mlsdm.com>
2022-01-17 00:07:35 +0100

    Terminado ejercicio 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function (...args) {
 
+    let contador = 0;
+    let error = "ERROR";
+
+    if (typeof args[0] != "number" || typeof args[1] != "number") {
+
+        return error;
+
+    } else if (args[0] < 0 || args[1] < 0) {
+
+        return error;
+
+    } else {
+
+        if (args[0] < args[1]) {
+
+            for (let i = args[0]; i <= args[1]; i++) {
+
+                contador = contador + i;
+
+            }
+
+        } else {
+
+            for (let i = args[0]; i >= args[1]; i--) {
+
+                contador = contador + i;
+
+            }
+        }
+
+        return contador;
+    }
 };
 
 // Do not edit below this line
