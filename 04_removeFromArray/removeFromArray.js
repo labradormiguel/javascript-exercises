436f5f3a9c73d1cec4b853a6bff9e7c9b7b85007
Miguel Labrador <github4138@mlsdm.com>
2022-01-16 22:43:12 +0100

    Terminado ejercicio 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...numero) {
 
+    for (let i = 0; i < numero.length; i++) {
+
+        let busqueda = array.indexOf(numero[i]);
+
+        if (busqueda >= 0)
+
+            array.splice(busqueda, 1);
+
+    }
+
+    return array
 };
 
 // Do not edit below this line
