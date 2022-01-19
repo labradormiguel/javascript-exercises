c84d5fe0f425eac7f7b84824523f3997a7487786
Miguel Labrador <github4138@mlsdm.com>
2022-01-19 21:48:16 +0100

    Terminado ejercicio 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function (anyo) {
+
+    if (anyo % 100 == 0 && anyo % 400 != 0) {
+
+        return false
+    }
+
+    else if (anyo % 4 == 0 || anyo % 100 == 0) {
+
+        return true
+
+    }
+
+    else {
+
+        return false
+
+    }
 
 };
 
